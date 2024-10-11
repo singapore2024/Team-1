9e23165f4363c2b471665169a7bb75bdc9ad2173
Fiona Li <fionalibq2407@gmail.com>
2024-10-11 15:55:31 +0800

    Combine inventory table with summary table

--- team1/src/app/page.js
+++ team1/src/app/page.js
@@ -1,10 +1,11 @@
 import Buttons from './components/Buttons';
-
+import Inventorypage from './inventory/page';
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Button Test Page</h1>
       <Buttons />
+      <Inventorypage />
     </div>
   );
 }
